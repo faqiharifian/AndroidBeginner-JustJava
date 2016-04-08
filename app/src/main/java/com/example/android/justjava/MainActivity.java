57b29004767cba0d15494fb098ee50d531fe75ec
Faqih Arifian <Faqih Arifian>
2016-04-08 22:32:25 +0700

    Combining String and Integer

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -31,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
-        displayMessage(priceMessage);
+        String message = "Total: $"+(quantity*5);
+        displayMessage(message);
     }
 
     /**
